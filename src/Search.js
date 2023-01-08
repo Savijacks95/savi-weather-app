203274347444c5d0f3c5d14a3e235231f3dc5b9f
Savijacks95 <savannahjackson95@gmail.com>
2023-01-08 15:34:01 -0800

    update search.js

--- src/Search.js
+++ src/Search.js
@@ -3,15 +3,17 @@ import React from "react";
 export default function Search() {
   return (
     <form id="search-form">
-      <div className="col-3 city">
-        <input
-          type="text"
-          className="form-control bg-transparent"
-          id="city"
-          placeholder="Search City"
-          autoFocus="on"
-        />
-        <button className="col-4 bg-transparent btn">GO</button>
+      <div className="container">
+        <div className="col-3 city">
+          <input
+            type="text"
+            className="form-control bg-transparent"
+            id="city"
+            placeholder="Search City"
+            autoFocus="on"
+          />
+          <button className="col-4 bg-transparent btn">GO</button>
+        </div>
       </div>
     </form>
   );
