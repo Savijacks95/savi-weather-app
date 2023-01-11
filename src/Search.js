2171a074371910b3bce9770ea72b04bfd6443dee
Savijacks95 <savannahjackson95@gmail.com>
2023-01-11 00:31:42 -0800

    updates to css and search.js

--- src/Search.js
+++ src/Search.js
@@ -4,19 +4,21 @@ export default function Search() {
   return (
     <div className="row">
       <div className="column city">
-        <div>
-          <p className="date">Sunday, 12</p>
+        <div className="card">
+          <form id="search-form">
+            <div>
+              <p className="date">Sunday, 12</p>
+            </div>
+            <input
+              type="text"
+              className="form-control bg-transparent"
+              id="city"
+              placeholder="Enter City"
+              autoFocus="on"
+            />
+          </form>
+          <button className="bg-transparent btn">GO</button>
         </div>
-        <form id="search-form">
-          <input
-            type="text"
-            className="form-control bg-transparent"
-            id="city"
-            placeholder="Enter City"
-            autoFocus="on"
-          />
-        </form>
-        <button className="bg-transparent btn">GO</button>
       </div>
     </div>
   );
