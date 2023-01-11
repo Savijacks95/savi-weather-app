35c1daa13c9b666d735ecdea41e0edd13dc8745e
Savijacks95 <savannahjackson95@gmail.com>
2023-01-11 08:59:13 -0800

    updates to css and link.js

--- src/link.js
+++ src/link.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Link() {
   return (
     <div className="link">
-      <a href="https://github.com/Savijacks95/savi-weather-app">
+      <a
+        href="https://github.com/Savijacks95/savi-weather-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Created by: Savannah Jackson
       </a>
     </div>
